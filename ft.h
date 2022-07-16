f3722461dfede95e616fda8c0a8f8aff8395672b
Fuqerduck <92591566+Fuqerduck@users.noreply.github.com>
2022-07-16 15:24:45 +0200

    Add files via upload

--- ft.h
+++ ft.h
@@ -11,3 +11,17 @@
 /* ************************************************************************** */
 
 /* this is just as a general repo for all the short functions*/
+#ifndef FT_H
+# define FT_H
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str);
+int	ft_solve(int **tab, int index);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int	ft_print_tab(int **tab);
+int	init_borders(int **tab, char *str);
+int	**init_tab(char *str);
+
+#endif
